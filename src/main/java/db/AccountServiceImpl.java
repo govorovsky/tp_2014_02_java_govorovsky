27844ae9c9c2c51561dd2d7763d797c80072b119
Andrew Govorovskij <govorovskij@gmail.com>
2014-04-04 22:48:01 +0400

    AccountServices share different connections. Fixed #6

--- src/main/java/db/AccountServiceImpl.java
+++ src/main/java/db/AccountServiceImpl.java
@@ -7,26 +7,43 @@ import exceptions.ExceptionMessages;
 import messageSystem.Address;
 import messageSystem.MessageSystem;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
  * Created by Andrew Govorovsky on 22.02.14
  */
 public class AccountServiceImpl implements AccountService {
-    private final UsersDAO dao;
+    private Connection connection;
     private final MessageSystem messageSystem;
     private final Address address = new Address();
 
     public AccountServiceImpl(MessageSystem ms) {
-        dao = new UsersDAO();
         this.messageSystem = ms;
         messageSystem.addService(this);
         messageSystem.getAddressService().addAddress(this);
     }
 
+    private Connection getDBConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                return connection;
+            }
+            DriverManager.registerDriver((Driver) Class.forName("com.mysql.jdbc.Driver").newInstance());
+            connection = DriverManager.getConnection("jdbc:mysql://" + "localhost:" + "3306/" + "gameserver?" + "user=root&" + "password=qazxsw12");
+            return connection;
+        } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
     @Override
     public void register(String username, String password) throws AccountServiceException, EmptyDataException {
+        UsersDAO dao = new UsersDAO(getDBConnection());
         try {
             checkLoginPassword(username, password);
             if (dao.getUser(username) != null) throw new AccountServiceException(ExceptionMessages.USER_ALREADY_EXISTS);
@@ -40,6 +57,7 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void delete(@NotNull String username) throws AccountServiceException {
+        UsersDAO dao = new UsersDAO(getDBConnection());
         try {
             if (!dao.deleteUser(username)) throw new AccountServiceException(ExceptionMessages.NO_SUCH_USER_FOUND);
         } catch (SQLException e) {
@@ -51,6 +69,7 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public long authenticate(String username, String password) throws AccountServiceException, EmptyDataException {
+        UsersDAO dao = new UsersDAO(getDBConnection());
         try {
             checkLoginPassword(username, password);
             UserDataSet user = dao.getUser(username);
