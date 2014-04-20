40d8e89d84b52e6c5095a8265e93ec628ac2eae7
Andrew Govorovskij <govorovskij@gmail.com>
2014-04-20 22:40:10 +0400

    DB resource leak fix. Fixed #14

--- src/main/java/db/Executor.java
+++ src/main/java/db/Executor.java
@@ -10,31 +10,22 @@ import java.sql.SQLException;
  */
 public class Executor {
     public static <T> T executeQuery(Connection connection, ExecHandler<T> handler, String query, Object... vars) throws SQLException {
-        PreparedStatement stm = connection.prepareStatement(query);
-        try {
+        try (PreparedStatement stm = connection.prepareStatement(query)) {
             for (int i = 0; i < vars.length; i++) {
                 stm.setObject(i + 1, vars[i]);
             }
-            ResultSet resultSet = stm.executeQuery();
-            try {
+            try (ResultSet resultSet = stm.executeQuery()) {
                 return handler.handle(resultSet);
-            } finally {
-                if (resultSet != null) resultSet.close();
             }
-        } finally {
-            if (stm != null) stm.close();
         }
     }
 
     public static int executeUpdate(Connection connection, String query, Object... vars) throws SQLException {
-        PreparedStatement stm = connection.prepareStatement(query);
-        try {
+        try (PreparedStatement stm = connection.prepareStatement(query)) {
             for (int i = 0; i < vars.length; i++) {
                 stm.setObject(i + 1, vars[i]);
             }
             return stm.executeUpdate();
-        } finally {
-            if (stm != null) stm.close();
         }
     }
 }
